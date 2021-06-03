991aca21f6ea738d7ae3bd43c97136b625b317b7
Joseva <josevahandika@gmail.com>
2021-06-03 15:45:28 +0700

    commit final - 3 juni

--- app/src/main/java/com/josevahandika/akb_mobile/api/TransaksiAPI.java
+++ app/src/main/java/com/josevahandika/akb_mobile/api/TransaksiAPI.java
@@ -1,7 +1,7 @@
 package com.josevahandika.akb_mobile.api;
 
 public class TransaksiAPI {
-    public static final String ROOT_URL   = "http://192.168.100.10:8000/";
+    public static final String ROOT_URL   = "https://akbp3leva.gerardoleonel.com/";
     public static final String ROOT_API   = ROOT_URL+ "api/";
     public static final String URL_UPDATE = ROOT_API+"transaksiBayar/";
     public static final String URL_FINISH = ROOT_API+"finishMobile/";
